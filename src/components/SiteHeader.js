2f9aeee3b8211876c1682ee8bd6849142c4cd6d1
Shawn Wilson <wilson.ohyeah@gmail.com>
2021-01-30 14:23:30 -0700

    Adds new SiteHeader component + storybook story

---
+++ src/components/SiteHeader.js
@@ -0,0 +1,21 @@
+const SiteHeader = ({ perRow, children }) => {
+  return (
+    <header className="h-16 pl-2 shadow-md bg-white">
+      <div className="content flex flex-row h-full">
+        <figure className="w-14 no-underline text-black flex flex-row items-center">
+          <img
+            src="/assets/images/mc-logo.png"
+            className="inline-block w-10 h-10 mr-2"
+            alt="Master Chef Compendium"
+          />
+          <span class="inline-block text-xs font-black font-branded">
+            Masterchef<br />
+            Compendium
+          </span>
+        </figure>
+      </div>
+    </header>
+  );
+};
+
+export default SiteHeader;
---
+++ src/stories/SiteHeader.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SiteHeader from '../components/SiteHeader';
+
+const story = {
+  title: 'Example/Site Header',
+  component: SiteHeader,
+};
+
+const Template = (args) => (
+  <div class="bg-gray-200 p-10">
+    <SiteHeader {...args}></SiteHeader>
+  </div>
+);
+
+export default story;
+
+export const Example = Template.bind({});
+Example.args = {};
